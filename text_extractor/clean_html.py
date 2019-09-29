a05ede3e836a168406ce0fd31781745078c47979
Nekocat299 <bouncing.cat.com@gmail.com>
2019-09-30 02:05:36 +0900

    [update] updated text_extractor\clean_html.py func:url2text(url)

--- text_extractor/clean_html.py
+++ text_extractor/clean_html.py
@@ -1,16 +1,30 @@
 import requests
 import lxml.html
+import re
 
 
-url = "http://www.gutenberg.org/files/1400/1400-h/1400-h.htm"
 def url2text(url):
     r = requests.get(url).text
     r = r.encode('utf-8')
     html = lxml.html.fromstring(r)
     elems = html.xpath('//p')
     elems = [i.text for i in elems if i.text is not None]
-    return ' '.join(elems)
+    return reduce_space(''.join(elems))
 
-with open("sample.txt", "w", encoding='utf-8') as file:
-    file.write(url2text(url))
+
+def reduce_space(data):
+    data = re.sub('\s+', ' ', data)
+    data = data.replace("\n\n", '')
+    return data
+
+"""
+使い方
+url = "http://www.gutenberg.org/files/1400/1400-h/1400-h.htm"
+data = url2text(url)
+print(data)
+でテキストデータ出てくるヨ
+
+"""
+
+
 
