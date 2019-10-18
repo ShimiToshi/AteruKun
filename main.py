8dcfb2c848b26b664a6bf461ad9d622112d3cd4f
DeskTop-Toshi <shimi.toshi421@gmail.com>
2019-10-18 16:59:00 +0900

    [add] test2 94% Data

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from mykeras import keras_negapozi as myk
 filename = "./DATA4train_test/labled_good_data.pickle"
 filename2 = "./DATA4train_test/labled_bad_data.pickle"
 
-# cls = myk.NegaPosi(filename, filename2)
-# cls.pre_convertdata()
-# cls.makemodel()
-# cls.start()
-#
-# cls.plotresult()
-# cls.resulttest()
-# cls.savemodel("test")
+cls = myk.NegaPosi(filename, filename2)
+cls.pre_convertdata()
+cls.makemodel()
+cls.start()
+
+cls.plotresult()
+cls.resulttest()
+cls.savemodel("test2")
 
 
 ## 学習済みはこっち
-cls = myk.NegaPosi(filename, None)
-cdata, clabel = cls.pre_convertdata()
-cls.loadmodel("test")
-
-# data テストしたいデータ
-# この場合、入れるデータ（パケットをコンバートしないといけない）
-cls.predict(cdata)
+# cls = myk.NegaPosi(filename, None)
+# cdata, clabel = cls.pre_convertdata()
+# cls.loadmodel("test")
+#
+# # data テストしたいデータ
+# # この場合、入れるデータ（パケットをコンバートしないといけない）
+# cls.predict(cdata)
